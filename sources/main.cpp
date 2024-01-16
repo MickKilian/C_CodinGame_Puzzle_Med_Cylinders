e31234531f39532f94b79af17a0de618b1d2fa8b
Michael's MacBook Pro 16 <michaelsmacbookpro16@MacBook-Pro-de-Michael-2.local>
2024-01-16 16:38:55 +0100

    final version passing all tests

--- sources/main.cpp
+++ sources/main.cpp
@@ -19,35 +19,10 @@ typedef struct s_cylinder
     int     radius;
 }   t_cylinder;
 
-void    printArrayInt(string text, const int *array, int size) {
-    cerr << text << " : ";
+void    copy_array(int *arr1, int size, int *arr2) {
     for (int i = 0; i < size; i++) {
-        cerr << array[i] << " ";
+        arr2[i] = arr1[i];
     }
-    cerr << endl;
-}
-
-void    printArrayCyl(string text, const t_cylinder *array, int size) {
-    cerr << text << " : ";
-    for (int i = 0; i < size; i++) {
-        cerr << "(" << array[i].position << ", " << array[i].radius << "), ";
-    }
-    cerr << endl;
-}
-
-void    printVector(const vector<int> vec) {
-    cerr << "Seq : ";
-    
-    for (int i = 0; i < (int)vec.size(); i++) {
-        cerr << vec[i] << " ";
-    }
-    cerr << endl;
-}
-
-double  distance(double r1, double r2) {
-    if (!(r1 * r2))
-        return (max(r1, r2));
-    return (2 * sqrt(r1 * r2));
 }
 
 vector<int> split(string str) {
@@ -80,18 +55,31 @@ bool    isUsed(int rank, const int *order, int position) {
     return (false);
 }
 
+void order_sequence(int *sequence, int size, const int *order) {
+    int  temp[size];
+
+    copy_array(sequence, size, temp);
+    for (int i = 0; i < size; i++)
+        sequence[i] = temp[order[i]];
+}
+
+double  distance(double r1, double r2) {
+    if (!(r1 * r2))
+        return (max(r1, r2));
+    return (2 * sqrt(r1 * r2));
+}
+
 int next_order(const int *sequence, int *order, int size) {
     int position = size - 1;
     int candidate;
     int curCandidate;
 
-//cerr << "inside next_order" << endl;
     while (position >= 0) {
         curCandidate = order[position];
         candidate = curCandidate;
         while (++candidate < size && (isUsed(candidate, order, position) || sequence[candidate] == sequence[curCandidate]
             || (candidate == size - 1 && (position < size / 2 || position == size - 1))
-            || (candidate > size / 2 + size % 2 && position < size -  1 && (order[position + 1] == candidate - 1 || order[position - 1] == candidate - 1))))
+            || (candidate > size / 2 + size % 2 && position < size -  1 && position > 0 && (order[position - 1] == candidate - 1))))
                 ;
         if (candidate > size - 1)
             position--;
@@ -110,7 +98,6 @@ int next_order(const int *sequence, int *order, int size) {
             order[position] = candidate;
         }
     }
-    //printArrayInt("Next order : ", order, size);
     return (0);
 }
 
@@ -118,19 +105,13 @@ void    compute_positions(t_cylinder *positions, int size, int n = 1) {
     double most_right_position = 0;
     double calc_position = 0;
 
-    if (n == size) {
+    if (n == size)
         return ;
-    }
-    //cerr << "       calculating position : " << n << " radius : " << positions[n].radius << endl;
     for (int i = 0; i < n; i++) {
         calc_position = positions[i].position + distance(positions[i].radius, positions[n].radius);
-        //cerr << "       : " << i << " (" << positions[i].position << " + " << distance(positions[i].radius, positions[n].radius) << " = " << calc_position << ")" << endl;
-        if (most_right_position == 0 || calc_position > most_right_position) {
-            //cerr << "       *** updating" << endl;
+        if (most_right_position == 0 || calc_position > most_right_position)
             most_right_position = calc_position;
-        }
     }
-    //cerr << "       *** final : " << most_right_position << endl;
     positions[n].position = most_right_position;
     compute_positions(positions, size, n + 1);
 }
@@ -139,7 +120,6 @@ double  compute_distance(const int *sequence, int size) {
     t_cylinder   positions[size + 2]; 
     t_cylinder cyl;
 
-    // add left wall
     cyl.position = 0.0;
     cyl.radius = 0;
     positions[0] = cyl;   
@@ -148,73 +128,39 @@ double  compute_distance(const int *sequence, int size) {
         cyl.radius = sequence[i];
         positions[i + 1] = cyl;
     }
-    // add right wall
     cyl.position = 0.0;
     cyl.radius = 0;
     positions[size + 1] = cyl;
 
     compute_positions(positions, size + 2);
-    //cerr << positions[size + 2].position << endl;
     return(positions[size + 1].position);
 }
 
-void    copy_array(int *arr1, int size, int *arr2) {
-    for (int i = 0; i < size; i++) {
-        arr2[i] = arr1[i];
-    }
-}
-
-void order_sequence(int *sequence, int size, const int *order) {
-    int  temp[size];
-
-    copy_array(sequence, size, temp);
-    for (int i = 0; i < size; i++)
-        sequence[i] = temp[order[i]];
-}
-
 double min_distance(int *original_sequence, int size) {
-    //int size = (int)(*original_sequence).size();
-    //vector<int> order(size, -1);
     int order[size];
     int sequence[size];
     int position = -1;
     double min_dist = 0;
     double cur_dist = 0;
-    //int count = 0;
 
     copy_array(original_sequence, size, sequence);
-    //cerr << "size sequence : " << size << endl;
-    //printArrayInt("original_sequence", original_sequence, size);
-    while (position < size - 1) {
-        position++;
-        //order[position] = position;
+    while (position++ < size - 1)
         order[position] = position;
-    }
     order_sequence(sequence, size, order);
-    //printVector(sequence);
     min_dist = compute_distance(sequence, size);
     while (!next_order(original_sequence, order, size)) {
         copy_array(original_sequence, size, sequence);
         order_sequence(sequence, size, order);
-        //printArrayInt("current_sequence", sequence, size);
         cur_dist = compute_distance(sequence, size);
-        //if ((int)cur_dist == 3775)
-        //        printArrayInt("Order min", order, size);
-        if (cur_dist < min_dist) {
+        if (cur_dist < min_dist)
             min_dist = cur_dist;
-        }
-        //count++;
     }
-    //cerr << "count : " << count << endl;
-    //order.clear();
     return (min_dist);
 }
 
 int main()
 { 
     int n;
-    //struct timeval start;
-    //struct timeval tp;
     double  distance = 0;
     vector<int> numbers;
     string line;
@@ -222,31 +168,19 @@ int main()
     cin >> n; cin.ignore();
     for (int i = 0; i < n; i++) {
         getline(cin, line);
-        //cerr << line << endl;
         numbers.clear();
         numbers = split(line);
         int nb_elem = numbers[0];
         numbers.erase(numbers.begin());
-        //printVector(numbers);
         sort(numbers.begin(), numbers.end());
         int num[nb_elem];
-        for (int i = 0; i < nb_elem; i++) {
+        for (int i = 0; i < nb_elem; i++)
             num[i] = numbers[i];
-        }
-        //cerr << "size numbers : " << nb_elem << endl;
-        //printArrayInt("input", num, nb_elem);
         if (nb_elem == 1)
             distance = 2.0 * num[0];
         else
             distance = min_distance(num, nb_elem);
         cout << setprecision(3) << fixed << distance << endl;
-        //gettimeofday(&start, NULL);
-        //gettimeofday(&tp, NULL);
-        //cerr << "step[" << i << "] : " << tp.tv_sec * 1000 + tp.tv_usec / 1000 - start.tv_sec * 1000 + start.tv_usec / 1000 << " ms" << endl;
         numbers.clear();
     }
-    // Write an answer using cout. DON'T FORGET THE "<< endl"
-    // To debug: cerr << "Debug messages..." << endl;
-
-    //cout << "answer" << endl;
 }
